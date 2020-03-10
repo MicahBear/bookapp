c3bd192294f9337925523a655c36d53301289e29
micahbear <micah.b.elm@gmail.com>
2020-03-10 10:52:07 -0700

    bug resolve

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const indexRouter = require("./routes.index");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-app.set("layout", "layous/layout");
+app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
